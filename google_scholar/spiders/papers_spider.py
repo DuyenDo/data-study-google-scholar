fc7e314e2348caeadc3371fb8558c179f127211e
Duyen Do <duyen.do2017@ict.jvn.edu.vn>
2019-12-23 12:17:54 +0100

    - Apply method 'get_path' to create path for both windows and linux environment
    - Add configuration to not open webdriver browser
    - Pass arguments from cmdline execute to spider
    - Re-factor code

--- google_scholar/spiders/papers_spider.py
+++ google_scholar/spiders/papers_spider.py
@@ -1,58 +1,62 @@
 import scrapy
 from scrapy.http import TextResponse
+from scrapy.utils.log import configure_logging
+
 from selenium import webdriver
-import os
-import csv
+
 import time
-import pickle
+import os
+import sys
+
+import csv
+
 import pandas as pd
 
+from utils.tools import get_path, write_csv, write_pickle, monitor_crawler
+
 class GSSpider(scrapy.Spider):
-    name = "papers"
+    name = "papers_2"
+
+    def __init__(self, input_file = ''):
+        self.ROOT_DIR = os.path.dirname(sys.modules['__main__'].__file__)
+        self.input_file = input_file
 
     def start_requests(self):
+        authors_path = get_path([self.ROOT_DIR, "data", "authors", self.input_file])
+        authors_df = pd.read_csv(authors_path)
+        
+        urls = ('https://scholar.google.com/citations?hl=en&user=' + authors_df['AuthorID'].astype(str))\
+                .to_list()[0:1]
 
-        path = os.path.join(os.path.dirname(__file__), '..\\..\\data\\')
-        dataframe = pd.read_csv("%sauthors_list-standford.csv" % path)
-        dataframe['AuthorID'] = 'https://scholar.google.com/citations?hl=en&user=' + dataframe['AuthorID'].astype(str)
-        urls = dataframe['AuthorID'].to_list()[169:500]
-
-        # # URLs of users
-        # urls = [
-        #     # "https://scholar.google.com/citations?hl=en&user=WJPjaDgAAAAJ",
-        #     # "https://scholar.google.com/citations?hl=en&user=YMfR1SIAAAAJ",
-        # ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-
-        # Path to chromedriver.exe
-        chromedriver_path = os.path.join(os.path.dirname(__file__), '..\\..\\libs\\chromedriver.exe')
-        # Create a webdriver
-        self.driver = webdriver.Chrome(chromedriver_path)
+        # Configure webdriver
+        chromedriver_path = get_path([self.ROOT_DIR, "libs", "chromedriver.exe"])
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path, options=op)
         self.driver.implicitly_wait(60)
         
-        # Get userId from user's URL
-        user = response.url.split("user=")[1].split("&")[0]
+        # Extract user_id
+        user_id = response.url.split("user=")[1].split("&")[0]
         
-        # Use webdriver to make request to a user's URL
+        # Make request
         self.driver.get(response.url)
 
-        # Create a variable for response from webdriver
-        clicked_response = None
+        response_after_click = None
 
-        # Click 'Show more' until this button is disabled to show full list of papers
+        # Click 'Show more' until the button is disabled
         while True:
             try:
-                # Wait 15 seconds for loading page before next click
+                # Wait 15 seconds before next click
                 time.sleep(15)
-                # Find button 'Show more' when it is not disabled
+                # Click button 'Show more' while it is not disabled
                 self.driver.find_element_by_xpath('//button[@id="gsc_bpf_more"][not(@disabled)]').click()
-            except Exception  as e:
-                print(e)
-                # When button 'Show more' is disabled, get response from webdriver
-                clicked_response = TextResponse(url=response.url, body=self.driver.page_source, encoding='utf-8')
+            except:
+                # Get response after clicking completely
+                response_after_click = TextResponse(url=response.url, body=self.driver.page_source, encoding='utf-8')
                 # Close webdriver
                 self.driver.close()
                 break
@@ -61,44 +65,40 @@ class GSSpider(scrapy.Spider):
         papers = []
 
         # Get all rows in the table from webdriver response, each paper is in a row
-        rows = clicked_response.xpath("//table[@id='gsc_a_t']//tr[@class='gsc_a_tr']")
+        rows = response_after_click.xpath("//table[@id='gsc_a_t']//tr[@class='gsc_a_tr']")
 
-        # In each paper, get title, authors, conference, url of citations, count of citations and year of publication
-        for row in rows:
-            title = row.xpath(".//td[@class='gsc_a_t']/a/text()").get()
-            authors = row.xpath(".//td[@class='gsc_a_t']/div[1]/text()").get()
-            conference = row.xpath(".//td[@class='gsc_a_t']/div[2]/text()").get()
-            cited_url = row.xpath(".//td[@class='gsc_a_c']/a[@class='gsc_a_ac gs_ibl']/@href").get()
-            cited_count = row.xpath(".//td[@class='gsc_a_c']/a/text()").get()
-            year = row.xpath(".//td[@class='gsc_a_y']/span/text()").get()
+        if (len(rows)>0):
+            # In each paper, get title, authors, conference, url of citations, count of citations and year of publication
+            for row in rows:
+                title = row.xpath(".//td[@class='gsc_a_t']/a/text()").get()
+                authors = row.xpath(".//td[@class='gsc_a_t']/div[1]/text()").get()
+                conference = row.xpath(".//td[@class='gsc_a_t']/div[2]/text()").get()
+                cited_url = row.xpath(".//td[@class='gsc_a_c']/a[@class='gsc_a_ac gs_ibl']/@href").get()
+                cited_count = row.xpath(".//td[@class='gsc_a_c']/a/text()").get()
+                year = row.xpath(".//td[@class='gsc_a_y']/span/text()").get()
 
-            cited_count = 0 if cited_count is None else int(cited_count)
-            year = 0 if year is None else int(year)
+                cited_count = 0 if cited_count is None else int(cited_count)
+                year = 0 if year is None else int(year)
 
-            # Create a dictionary for each paper
-            paper = {
-                'Title': title,
-                'Authors': authors,
-                'Platform': conference,
-                'Cited_url': cited_url,
-                'Cited_count': cited_count,
-                'Year': year
-            }
+                # Create a dictionary for each paper
+                paper = {
+                    'Title': title,
+                    'Authors': authors,
+                    'Platform': conference,
+                    'Cited_url': cited_url,
+                    'Cited_count': cited_count,
+                    'Year': year
+                }
 
-            papers.append(paper)
-
-        data_folder = os.path.join(os.path.dirname(__file__), '..\\..\\data\\papers_standford\\')
-        file_user_papers = '%spapers-of-authorID-%s.csv' % (data_folder, user)
-
-        # Write to csv file
-        keys = papers[0].keys()
-        with open(file_user_papers, 'w', encoding='utf-8', newline='') as output_file:
-            dict_writer = csv.DictWriter(output_file, keys)
-            dict_writer.writeheader()
-            dict_writer.writerows(papers)
-
-        # Write to pickle
-        filename = "{}papers-of-authorID-{}.pkl".format(data_folder, user)
-        with open(filename, "wb") as pfile:
-            pickle.dump(paper, pfile)
-    
+                papers.append(paper)
+            
+            # Output
+            organization = self.input_file.split('.')[0].replace("authors_", "")
+            papers_csv = get_path([self.ROOT_DIR,\
+                                "data", "papers", organization, "papers-of-authorID-{}.csv".format(user_id)])
+            write_csv(papers, papers_csv)
+            papers_pkl = get_path([self.ROOT_DIR, \
+                                "data", "papers", organization, "papers-of-authorID-{}.pkl".format(user_id)])
+            write_pickle(papers, papers_pkl)
+            monitor_file = get_path([self.ROOT_DIR, "data", "monitors", 'crawled_' + self.input_file])
+            monitor_crawler(monitor_file, user_id)
