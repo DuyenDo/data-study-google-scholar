bdefad6bfeb61d89bb81040aaf9b3de2a03f6c0f
Duyen Do <duyen.do2017@ict.jvn.edu.vn>
2019-12-23 15:18:03 +0100

    - Read author's URLs from file
    - Add configuration to not open webdriver browser
    - Monitor crawled URLs
    - Re-factor code

--- google_scholar/spiders/authors.py
+++ google_scholar/spiders/authors.py
@@ -1,102 +1,96 @@
 import scrapy
 from scrapy.http import TextResponse
+
 from selenium import webdriver
+
 import os
-import csv
-import logging
+import sys
 import time
 
-logging.basicConfig(filename=os.path.join(os.path.dirname(__file__), '..\\..\\logs\\authors.log'),\
-                    level=logging.ERROR)
-logger = logging.getLogger(__name__)
+import pandas as pd
+
+from utils.tools import get_path, write_csv, write_pickle, monitor_crawler
 
 class GSSpider(scrapy.Spider):
     name = "authors"
 
+    def __init__(self):
+        self.ROOT_DIR = os.path.dirname(sys.modules['__main__'].__file__)
+
     def start_requests(self):
-        # Get authors verified by the given email domain name
-        urls = [
-            # 'https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=%s' % 'eurecom.fr',
-            "https://scholar.google.com/citations?view_op=view_org&hl=en&org=8539678734835078480&fbclid=IwAR0AN_dLcYIL4IW5XspJ_BjnrI91SYQ3RIWiUi8agVL0X7ZAeKiXxOmSyCU"
-        ]
+        
+        organizations_path = get_path([self.ROOT_DIR, "data", "organizations.txt"])
+        organizations_df = pd.read_csv(organizations_path)
+        urls = organizations_df['URL'].to_list()
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-
-        # Path to chromedriver.exe
-        chromedriver_path = os.path.join(os.path.dirname(__file__), '..\\..\\libs\\chromedriver.exe')
-        # Create a webdriver
-        self.driver = webdriver.Chrome(chromedriver_path)
-        # Wait 60 seconds for loading page if the element is not available yet
+        # Configure webdriver
+        chromedriver_path = get_path([self.ROOT_DIR, "libs", "chromedriver.exe"])
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path, options=op)
         self.driver.implicitly_wait(60)
 
-        # Tracking the current url
-        current_url = response.url
+        # Extract organization id
+        org_id = response.url.split("org=")[1].split("&")[0]
 
-        # Create a list of authors who are verified by the given email domain name
+        # List of authors of the organization
         authors = []
+        monitor_url = ''
 
-        # Use webdriver to make request to a user's URL
+        # Make request
         self.driver.get(response.url)
 
-        # Create a variable for response from webdriver
+        # First page of the request
         page_response = TextResponse(url=response.url, body=self.driver.page_source, encoding='utf-8')
 
         self.parse_one_page(page_response, authors)
 
-        # Click 'Next page' until this button is disabled
+        # Click 'Next page' and parse until this button is disabled
         while True:
             try:
+                # Wait 15s before next click
+                time.sleep(15)
                 # Find button 'Next' when it is not hidden
                 self.driver.find_element_by_xpath("//div[@id='gsc_authors_bottom_pag']//button[@aria-label='Next'][not(@disabled)]").click()
-
                 # Create a variable for response from webdriver
                 next_page_response = TextResponse(url=self.driver.current_url, body=self.driver.page_source, encoding='utf-8')
-
                 self.parse_one_page(next_page_response, authors)
-
-                current_url = self.driver.current_url
-
-                # Wait 60 seconds before next click
-                time.sleep(15)
-
-            except Exception as e: 
-                logger.info(current_url)
-                logger.error(e)
-
-                # Close webdriver
+                
+                monitor_url = self.driver.current_url
+            except:
                 self.driver.close()
                 break
 
-        data_folder = os.path.join(os.path.dirname(__file__), '..\\..\\data\\')
-        # Create a filename
-        file_cited_papers = '%sauthors_list-%s.csv' % (data_folder, 'standford_2')
-        # Write list of papers to csv file
-        keys = authors[0].keys()
-        with open(file_cited_papers, 'w', encoding='utf-8', newline='') as output_file:
-            dict_writer = csv.DictWriter(output_file, keys)
-            dict_writer.writeheader()
-            dict_writer.writerows(authors)
+        # Output
+        if len(authors) > 0:
+            csv_path = get_path([self.ROOT_DIR, "data", "authors", "authors_orgID_{}.csv".format(org_id)])
+            write_csv(authors, csv_path)
+            
+            pkl_path = get_path([self.ROOT_DIR, "data", "authors", "authors_orgID_{}.pkl".format(org_id)])
+            write_pickle(authors, pkl_path)
+            
+            monitor_file = get_path([self.ROOT_DIR, "data", "monitors", 'crawled_organizations.txt'])
+            monitor_crawler(monitor_file, monitor_url)
 
-    
     def parse_one_page(self, page_response, author_list):
 
         rows = page_response.xpath("//div[@class='gs_ai_t']")
 
-        for row in rows:
-            author_id = row.xpath(".//h3[@class='gs_ai_name']/a/@href").get()\
-                            .split("user=")[1].split("&")[0]
-            author_name = row.xpath(".//h3[@class='gs_ai_name']/a/text()").get()
-            cited_by_str = row.xpath(".//div[@class='gs_ai_cby']/text()").get()\
-                            .replace("Cited by ", "")
-            cited_by_int = int(cited_by_str) if cited_by_str.isdigit() else 0
-            # Create a dictionary for each paper
-            
-            author = {
-                'AuthorID': author_id,
-                'AuthorName': author_name,
-                'Count_cited': cited_by_int
-            }
+        if len(rows) > 0:
+            for row in rows:
+                author_id = row.xpath(".//h3[@class='gs_ai_name']/a/@href").get().split("user=")[1].split("&")[0]
+                author_name = row.xpath(".//h3[@class='gs_ai_name']/a/text()").get()
+                cited_by_str = row.xpath(".//div[@class='gs_ai_cby']/text()").get().replace("Cited by ", "")
+                cited_by_int = int(cited_by_str) if cited_by_str.isdigit() else 0
+                
+                author = {
+                    'AuthorID': author_id,
+                    'AuthorName': author_name,
+                    'Count_cited': cited_by_int
+                }
 
-            author_list.append(author)
+                author_list.append(author)
