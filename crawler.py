6003f31ba0c9a21770058d3b0008f1908d1c3889
Duyen Do <duyen.do2017@ict.jvn.edu.vn>
2019-12-24 16:30:11 +0100

    - Apply method 'get_path' to create path for both windows and linux environment
    - Add configuration to not open webdriver browser
    - Pass arguments from cmdline execute to spider
    - Re-factor code

--- crawler.py
+++ crawler.py
@@ -3,17 +3,17 @@ from scrapy.cmdline import execute
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-try:
-    execute(
-        [
-            'scrapy',
-            'crawl',
-            'papers',
-            '-a', 'input_file=authors_orgID_8539678734835078480.csv'
-        ]
-    )
-except SystemExit:
-    pass
+# try:
+#     execute(
+#         [
+#             'scrapy',
+#             'crawl',
+#             'papers',
+#             '-a', 'input_file=authors_orgID_8539678734835078480.csv'
+#         ]
+#     )
+# except SystemExit:
+#     pass
 
 # try:
 #     execute(
@@ -25,3 +25,19 @@ except SystemExit:
 #     )
 # except SystemExit:
 #     pass
+
+try:
+    execute(
+        [
+            'scrapy',
+            'crawl',
+            'citations',
+            '-a', 'input_dir=authors_orgID_7868974552293588111',
+            '-a', 'input_file=papers-of-authorID-_b6dDHMAAAAJ.csv'
+            # if arg 'input_file' is specified: read 'data/papers/<input_dir>/<input_file>.csv'
+            # else: read 'data/papers/input_dir/*.csv'
+            
+        ]
+    )
+except SystemExit:
+    pass
